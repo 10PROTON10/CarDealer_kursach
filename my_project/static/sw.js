be48def824fdf10025905451d405baeddf7f692c
10PROTON10 <antosha.maslakov98@gmail.com>
2024-03-18 23:18:31 +0300

    Create sw.js

---
+++ my_project/static/sw.js
@@ -0,0 +1,42 @@
+// service-worker.js
+const CACHE_NAME = 'my-cache';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  'http://127.0.0.1:8000/'
+
+  // Добавьте другие ресурсы вашего приложения
+];
+
+// Установка Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Обработка событий fetch
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
